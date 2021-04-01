c8a079d3d7b1cfb758386dd715884a30a4a0df48
carson <carson@leaddyno.com>
2021-03-31 20:32:34 -0600

    scratch

---
+++ src/test/java/edu/montana/csci/csci468/demo/Scratch.java
@@ -0,0 +1,25 @@
+package edu.montana.csci.csci468.demo;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Scratch {
+
+    public void main() {
+        var x = 10;
+        var y = 20;
+        var z = x + y;
+        var q = x + y;
+
+    }
+
+    int add(int i) {
+        return i + 13;
+    }
+
+    public int intFunc(int i1, int i2) {
+        return i1 + i2;
+    }
+
+}
