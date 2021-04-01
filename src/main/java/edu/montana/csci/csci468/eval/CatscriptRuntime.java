758527663e6d6750f282c7b8520cba2151bd5be0
carson <carson@leaddyno.com>
2021-03-31 20:31:42 -0600

    runtime stuff

--- src/main/java/edu/montana/csci/csci468/eval/CatscriptRuntime.java
+++ src/main/java/edu/montana/csci/csci468/eval/CatscriptRuntime.java
@@ -1,30 +1,46 @@
 package edu.montana.csci.csci468.eval;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
 
 // TODO - implement proper scoping
 public class CatscriptRuntime {
     LinkedList<Map<String, Object>> scopes = new LinkedList<>();
-    HashMap<String, Object> globalScope;
 
     public CatscriptRuntime(){
-        globalScope = new HashMap<>();
+        HashMap<String, Object> globalScope = new HashMap<>();
+        scopes.push(globalScope);
     }
 
     public Object getValue(String name) {
-        return globalScope.get(name);
+        Iterator<Map<String, Object>> mapIterator = scopes.descendingIterator();
+        while (mapIterator.hasNext()) {
+            Map<String, Object> scope = mapIterator.next();
+            if (scope.containsKey(name)) {
+                return scope.get(name);
+            }
+        }
+        return null;
     }
 
     public void setValue(String variableName, Object val) {
-        globalScope.put(variableName, val);
+        for (Map<String, Object> scope : scopes) {
+            if (scope.containsKey(variableName)) {
+                scope.put(variableName, val);
+                return;
+            }
+        }
+        scopes.peekLast().put(variableName, val);
     }
 
     public void pushScope() {
+        scopes.push(new HashMap<>());
     }
 
     public void popScope() {
+        scopes.pop();
     }
 
 }
